71c3eaeb2b44469e44e5756860d13ea6463c5c8b
Matthias-Carre <lemattheuxbis@gmail.com>
2025-02-06 09:07:01 +0100

    ajout d'une balle qui roule

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import {CitronModel} from "./Citron.js"
 let canvas = document.getElementById("maCanvas");
 let engine = new Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true, disableWebGL2Support: false });
 globalThis.HK = await HavokPhysics();
+let forceDirection;
+let spherePhysics;
+let sphere;
+
 
 const createScene = async function () {
     const scene = new Scene(engine);
@@ -17,7 +21,7 @@ const createScene = async function () {
     const havokInst = await HavokPhysics();
     const physics = new HavokPlugin(true, havokInst);
     scene.enablePhysics(new Vector3(0, -9.81, 0), physics);
-    const ground = MeshBuilder.CreateGround("ground", { width: 20, height: 20 }, scene);
+    const ground = MeshBuilder.CreateGround("ground", { width: 50, height: 50 }, scene);
     ground.position = new Vector3(0, -15, 0);
 
     let groundPhysics;
@@ -33,6 +37,53 @@ const createScene = async function () {
     const camera = new FreeCamera("camera", new Vector3(-100, 10, 0), scene);
     camera.setTarget(Vector3.Zero());
     camera.attachControl(canvas, true);
+
+    // Create sphere with physics
+    sphere = MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene);
+    sphere.position.y = 5;
+    spherePhysics = new PhysicsAggregate(sphere, PhysicsShapeType.SPHERE, { mass: 1 }, scene);
+
+    // Variables to track the current force
+    forceDirection = new Vector3(0, 0, 0);
+
+    // Add keyboard controls
+    window.addEventListener("keydown", (event) => {
+        const forceMagnitude = 40;
+        switch (event.key) {
+            case "w":
+                forceDirection.z = forceMagnitude;
+                break;
+            case "s":
+                forceDirection.z = -forceMagnitude;
+                break;
+            case "a":
+                forceDirection.x = -forceMagnitude;
+                break;
+            case "d":
+                forceDirection.x = forceMagnitude;
+                break;
+        }
+    });
+
+    window.addEventListener("keyup", (event) => {
+        switch (event.key) {
+            case "w":
+            case "s":
+                forceDirection.z = 0;
+                spherePhysics.body.setLinearVelocity(Vector3.Zero());
+                break;
+            case "a":
+            case "d":
+                
+                forceDirection.x = 0;
+                spherePhysics.body.setLinearVelocity(Vector3.Zero());
+                break;
+
+        }
+    });
+
+
+
     return scene;
 
 
@@ -41,6 +92,9 @@ const createScene = async function () {
 createScene().then((scene) => {
     engine.runRenderLoop(function () {
         if (scene) {
+            if (forceDirection.length() > 0) {
+                spherePhysics.body.applyForce(forceDirection, sphere.getAbsolutePosition());
+            }
             scene.render();
         }
     });
