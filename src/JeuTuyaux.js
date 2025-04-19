c79d51baeed5fcb9e181b9ef4269544abb4061df
Matthias Carré <lemattheuxbis@gmail.com>
2025-04-19 20:13:48 +0200

    jeu tuyaux

--- src/JeuTuyaux.js
+++ src/JeuTuyaux.js
@@ -1,51 +1,53 @@
-import { SceneLoader, Vector3,KeyboardEventTypes } from "@babylonjs/core";
+import { SceneLoader, Vector3, KeyboardEventTypes } from "@babylonjs/core";
 
 import TuyauDV from "./../assets/TuyauDroitVide.glb";
 import TuyauDP from "./../assets/TuyauDroitPlein.glb";
 import TuyauAV from "./../assets/TuyauAngleVide.glb";
 import TuyauAP from "./../assets/TuyauAnglePlein.glb";
+import EauMap from "./../assets/SolEau.glb";
 
 export class JeuTuyaux {
     constructor(scene) {
         this.scene = scene;
         this.models = [];
-        this.modelsP=[];
-        this.tuyauphy = null;
-        this.matrice = [[2,1,1,1,2,0,0,0],
-                        [2,1,2,0,2,2,0,0],
-                        [0,0,1,0,0,1,0,0],
-                        [2,0,2,2,0,1,0,0],
-                        [2,2,0,1,2,2,0,2],
-                        [0,1,0,1,1,0,0,1],
-                        [2,2,0,1,2,1,1,2],
-                        [2,1,1,2,0,0,0,0]];
+        this.modelsP = [];
+        this.eau=null;
+        this.eauvisi = false;
+        this.matrice = [[2, 1, 1, 1, 2, 0, 0, 0],
+        [2, 1, 2, 0, 2, 2, 0, 0],
+        [0, 0, 1, 0, 0, 1, 0, 0],
+        [2, 0, 2, 2, 0, 1, 0, 0],
+        [2, 2, 0, 1, 2, 2, 0, 2],
+        [0, 1, 0, 1, 1, 0, 0, 1],
+        [2, 2, 0, 1, 2, 1, 1, 2],
+        [2, 1, 1, 2, 0, 0, 0, 0]];
 
-        this.rotations=[[1,1,1,1,1,0,0,0],
-                        [1,1,1,0,1,1,0,0],
-                        [0,0,1,0,0,1,0,0],
-                        [1,0,1,1,0,1,0,0],
-                        [1,1,0,1,1,1,0,1],
-                        [0,1,0,1,1,0,0,1],
-                        [1,1,0,1,1,1,1,1],
-                        [1,1,1,1,0,0,0,0]];
+        this.rotations = [[1, 1, 1, 1, 1, 0, 0, 0],
+        [1, 1, 1, 0, 1, 1, 0, 0],
+        [0, 0, 1, 0, 0, 1, 0, 0],
+        [1, 0, 1, 1, 0, 1, 0, 0],
+        [1, 1, 0, 1, 1, 1, 0, 1],
+        [0, 1, 0, 1, 1, 0, 0, 1],
+        [1, 1, 0, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 0, 0, 0, 0]];
         //si 1-4 c'est un coin si 5 c'est un droit impaire et si 6 c'est un droit pair
         //5 gauche droite|6haut bas|1gauche haut|2droite haut|3bas droite|4bas gauche 
-        this.res=
-                [[3,5,5,5,4,0,0,0],
-                [2,5,4,0,2,4,0,0],
-                [0,0,6,0,0,6,0,0],
-                [4,0,2,4,0,6,0,0],
-                [2,4,0,6,3,1,0,3],
-                [0,6,0,6,6,0,0,6],
-                [3,1,0,6,2,5,5,1],
-                [2,5,5,1,0,0,0,0]];
+        this.res =
+            [[3, 5, 5, 5, 4, 0, 0, 0],
+            [2, 5, 4, 0, 2, 4, 0, 0],
+            [0, 0, 6, 0, 0, 6, 0, 0],
+            [4, 0, 2, 4, 0, 6, 0, 0],
+            [2, 4, 0, 6, 3, 1, 0, 3],
+            [0, 6, 0, 6, 6, 0, 0, 6],
+            [3, 1, 0, 6, 2, 5, 5, 1],
+            [2, 5, 5, 1, 0, 0, 0, 0]];
 
-        this.chemin = [[3,0],[4,0],[4,1],[5,1],[6,1],[6,0],[7,0],[7,1],[7,2],
-                [7,3],[6,3],[5,3],[4,3],[3,3],[3,2],[2,2],[1,2],[1,1],[1,0],
-                [0,0],[0,1],[0,2],[0,3],[0,4],[1,4],[1,5],[2,5],[3,5],[4,5],
-                [4,4],[5,4],[6,4],[6,5],[6,6],[6,7],[5,7],[4,7]];
+        this.chemin = [[3, 0], [4, 0], [4, 1], [5, 1], [6, 1], [6, 0], [7, 0], [7, 1], [7, 2],
+        [7, 3], [6, 3], [5, 3], [4, 3], [3, 3], [3, 2], [2, 2], [1, 2], [1, 1], [1, 0],
+        [0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [1, 4], [1, 5], [2, 5], [3, 5], [4, 5],
+        [4, 4], [5, 4], [6, 4], [6, 5], [6, 6], [6, 7], [5, 7], [4, 7]];
         this.KeyControles();
-        this.log=console.log("JeuTuyaux");
+
     }
 
     KeyControles() {
@@ -65,26 +67,28 @@ export class JeuTuyaux {
 
 
 
-    async createFromMatrice(positionInit){
+    async createFromMatrice(positionInit) {
         let vec = new Vector3(20, 20, 20);
         let coef = 20;
-        this.loadModel(TuyauDP, vec, new Vector3(positionInit.x+3*coef, positionInit.y, positionInit.z-coef), new Vector3(0, Math.PI, 0),positionInit,true,false);
-        for(let i = 0; i < this.matrice.length; i++) {
+        this.loadLac();
+        this.loadModel(TuyauDP, vec, new Vector3(positionInit.x + 3 * coef, positionInit.y, positionInit.z - coef), new Vector3(0, Math.PI, 0), positionInit, true, false);
+        for (let i = 0; i < this.matrice.length; i++) {
             for (let j = 0; j < this.matrice[i].length; j++) {
                 if (this.matrice[i][j] == 1) {
-                    this.loadModel(TuyauDV, vec, new Vector3(positionInit.x + i * coef, positionInit.y, positionInit.z + j * coef), new Vector3(0, 0, 0),positionInit,true,true);
-                    this.loadModel(TuyauDP, vec, new Vector3(positionInit.x + i * coef, positionInit.y, positionInit.z + j * coef), new Vector3(0, (this.res[i][j]-1)* (Math.PI/2), 0),positionInit,false,true);
-                }else if (this.matrice[i][j] == 2) {
-                    this.loadModel(TuyauAV, vec, new Vector3(positionInit.x + i * coef, positionInit.y, positionInit.z + j * coef), new Vector3(0, Math.PI / 2, 0),positionInit,true,true);
-                    this.loadModel(TuyauAP, vec, new Vector3(positionInit.x + i * coef, positionInit.y, positionInit.z + j * coef), new Vector3(0, this.res[i][j]*(Math.PI / 2), 0),positionInit,false,true);
+                    this.loadModel(TuyauDV, vec, new Vector3(positionInit.x + i * coef, positionInit.y, positionInit.z + j * coef), new Vector3(0, 0, 0), positionInit, true, true);
+                    this.loadModel(TuyauDP, vec, new Vector3(positionInit.x + i * coef, positionInit.y, positionInit.z + j * coef), new Vector3(0, (this.res[i][j] - 1) * (Math.PI / 2), 0), positionInit, false, true);
+                } else if (this.matrice[i][j] == 2) {
+                    this.loadModel(TuyauAV, vec, new Vector3(positionInit.x + i * coef, positionInit.y, positionInit.z + j * coef), new Vector3(0, Math.PI / 2, 0), positionInit, true, true);
+                    this.loadModel(TuyauAP, vec, new Vector3(positionInit.x + i * coef, positionInit.y, positionInit.z + j * coef), new Vector3(0, this.res[i][j] * (Math.PI / 2), 0), positionInit, false, true);
                 }
             }
         }
     }
 
 
-    async loadModel(model, scale, position, rotation,posInit,visibility,flip) {
+    async loadModel(model, scale, position, rotation, posInit, visibility, flip) {
         try {
+            this.eau = await this.loadLac();
             const result = await SceneLoader.ImportMeshAsync("", model, "", this.scene);
             this.model = result.meshes[0];
             this.model.scaling = new Vector3(scale.x, scale.y, scale.z);
@@ -96,14 +100,14 @@ export class JeuTuyaux {
             //permet de gerer avec les matrice leur pos et rota
             this.model.metadata = {
                 modelType: model,
-                gridPosition:{
-                    x:Math.floor((position.x-posInit.x)/20),
-                    z:Math.floor((position.z-posInit.z)/20)
+                gridPosition: {
+                    x: Math.floor((position.x - posInit.x) / 20),
+                    z: Math.floor((position.z - posInit.z) / 20)
                 }//on soutrais pour avoir des valeur quon peut utilisr dans la matrice
             };
-            if(visibility && flip){
+            if (visibility && flip) {
                 this.models.push(this.model);
-            }else if(!visibility && flip){
+            } else if (!visibility && flip) {
                 this.modelsP.push(this.model);
             }
             //this.applyTexture(texturePath);
@@ -111,30 +115,30 @@ export class JeuTuyaux {
             console.error("Error loading model:", error);
         }
     }
-    Closest(position){
+    Closest(position) {
         let closest = null;
         let min = Infinity;
-        for(const tuyau of this.models){
+        for (const tuyau of this.models) {
             const distance = Vector3.Distance(tuyau.position, position);
-            if(distance<min){
+            if (distance < min) {
                 min = distance;
                 closest = tuyau;
             }
         }
         return closest;
     }
-    rotateClosestTuyau(position){
+    rotateClosestTuyau(position) {
         const closest = this.Closest(position);
-        console.log("closest=",closest.metadata.gridPosition);
+        console.log("closest=", closest.metadata.gridPosition);
         //console.log("gridpos=",closest.metadata.gridPosition);
-        if(closest){
+        if (closest) {
 
             closest.rotation.y += Math.PI / 2;
             //console.log("rota=",this.rotations[closest.metadata.gridPosition.x][closest.metadata.gridPosition.z]);
-            if(this.rotations[closest.metadata.gridPosition.x][closest.metadata.gridPosition.z] == 4){
+            if (this.rotations[closest.metadata.gridPosition.x][closest.metadata.gridPosition.z] == 4) {
                 this.rotations[closest.metadata.gridPosition.x][closest.metadata.gridPosition.z] = 1;
-            }else{
-                this.rotations[closest.metadata.gridPosition.x][closest.metadata.gridPosition.z] += 1 ;
+            } else {
+                this.rotations[closest.metadata.gridPosition.x][closest.metadata.gridPosition.z] += 1;
                 //console.log("matrice=",this.rotations[closest.metadata.gridPosition.x][closest.metadata.gridPosition.z]);
             }
         }
@@ -143,21 +147,21 @@ export class JeuTuyaux {
     }
 
     //fonction de test je la laisse au cas ou
-    waterPropa(x,y){
+    waterPropa(x, y) {
         console.log("waterpropa");
-        
-        for(let i = 0; i < this.rotations.length; i++) {
+
+        for (let i = 0; i < this.rotations.length; i++) {
             for (let j = 0; j < this.rotations[i].length; j++) {
                 console.log(this.rotations[i][j], this.res[i][j]);
-                if (this.rotations[i][j] == this.res[i][j] || (this.res[i][j] > 4 && this.res[i][j]%2 == this.rotations[i][j]%2)) {
+                if (this.rotations[i][j] == this.res[i][j] || (this.res[i][j] > 4 && this.res[i][j] % 2 == this.rotations[i][j] % 2)) {
                     console.log("visible");
-                    for(let k = 0; k < this.modelsP.length; k++) {
-                        if(this.modelsP[k].metadata.gridPosition.x == i && this.modelsP[k].metadata.gridPosition.z == j){
-                            
-                            for(let mesh of this.modelsP[k].getChildMeshes()) {
+                    for (let k = 0; k < this.modelsP.length; k++) {
+                        if (this.modelsP[k].metadata.gridPosition.x == i && this.modelsP[k].metadata.gridPosition.z == j) {
+
+                            for (let mesh of this.modelsP[k].getChildMeshes()) {
                                 mesh.isVisible = true;
                             }
-                            
+
                         }
                     }
                 }
@@ -166,14 +170,14 @@ export class JeuTuyaux {
     }
 
     //retourne les coo des tuyaux bien placés
-    tuplesCorrecte(){
+    tuplesCorrecte() {
         var valides = [];
-        for(let i = 0; i < this.chemin.length; i++){
+        for (let i = 0; i < this.chemin.length; i++) {
             let tuple = this.chemin[i];
-            if(this.rotations[tuple[0]][tuple[1]] == this.res[tuple[0]][tuple[1]] || (this.res[tuple[0]][tuple[1]] > 4 && this.res[tuple[0]][tuple[1]]%2 == this.rotations[tuple[0]][tuple[1]]%2)){
+            if (this.rotations[tuple[0]][tuple[1]] == this.res[tuple[0]][tuple[1]] || (this.res[tuple[0]][tuple[1]] > 4 && this.res[tuple[0]][tuple[1]] % 2 == this.rotations[tuple[0]][tuple[1]] % 2)) {
                 //console.log("flow");
                 valides.push(tuple);
-            }else{
+            } else {
                 //console.log("no flow");
                 //console.log("valides=",valides);
                 return valides;
@@ -184,47 +188,84 @@ export class JeuTuyaux {
     }
 
     //change la visibilite des tuyaux bien et mal places
-    changeVisibility(){
+    changeVisibility() {
         let valides = this.tuplesCorrecte();
-        for(let i = 0; i < this.modelsP.length; i++) {
+
+        //apparition de leau 
+        if(valides.length == this.chemin.length && !this.eauvisi){
+            this.rempliLac();
+        }else if(this.eauvisi){
+            this.videLac();
+        }
+
+        for (let i = 0; i < this.modelsP.length; i++) {
             //console.log("tuple=",this.modelsP[i].metadata.gridPosition,"onValide?=",this.modelsP[i].metadata.gridPosition in valides);
             let tuple = this.modelsP[i].metadata.gridPosition;
             let presence = false;
-            for(let possi of valides){
-                if(tuple.x == possi[0] && tuple.z == possi[1]){
+            for (let possi of valides) {
+                if (tuple.x == possi[0] && tuple.z == possi[1]) {
                     //console.log("on rend visible ",tuple);
                     presence = true;
-                    for(let mesh of this.modelsP[i].getChildMeshes()) {
+                    for (let mesh of this.modelsP[i].getChildMeshes()) {
                         mesh.isVisible = true;
                     }
                 }
             }
-            if(!presence){
+            if (!presence) {
                 //console.log("on rend invisible ",tuple);
-                for(let mesh of this.modelsP[i].getChildMeshes()) {
+                for (let mesh of this.modelsP[i].getChildMeshes()) {
                     mesh.isVisible = false;
                 }
             }
         }
 
-        for(let i = 0; i < this.models.length; i++) {
+        for (let i = 0; i < this.models.length; i++) {
             let tuple = this.models[i].metadata.gridPosition;
             let presence = false;
-            for(let possi of valides){
-                if(tuple.x == possi[0] && tuple.z == possi[1]){
+            for (let possi of valides) {
+                if (tuple.x == possi[0] && tuple.z == possi[1]) {
                     //console.log("on rend invisible ",tuple);
                     presence = true;
-                    for(let mesh of this.models[i].getChildMeshes()) {
+                    for (let mesh of this.models[i].getChildMeshes()) {
                         mesh.isVisible = false;
                     }
                 }
             }
-            if(!presence){
+            if (!presence) {
                 //console.log("on rend visible ",tuple);
-                for(let mesh of this.models[i].getChildMeshes()) {
+                for (let mesh of this.models[i].getChildMeshes()) {
                     mesh.isVisible = true;
                 }
             }
         }
+    }
+    async loadLac(){
+        try {
+            const result = await SceneLoader.ImportMeshAsync("", EauMap, "", this.scene);
+            const ground = result.meshes[0];
+            result.meshes.forEach((mesh) => {
+                mesh.scaling = new Vector3(7, 7, 7);
+                mesh.isVisible = false;
+            });
+            ground.scaling = new Vector3(15, 15, 15);
+            ground.position = new Vector3(0, 0, 0);
+            ground.rotation = new Vector3(0, Math.PI / 2, 0);
+            return result;
+        } catch (error) {
+            console.error("Erreur lors du chargement du lac:", error);
+            return { meshes: [] };
+        }
+    }
+    async rempliLac() {
+        this.eauvisi = true;
+        this.eau.meshes.forEach((mesh) => {
+            mesh.isVisible = true;
+        });
+    }
+    async videLac() {
+        this.eauvisi = false;
+        this.eau.meshes.forEach((mesh) => {
+            mesh.isVisible = false;
+        });
     }
 }
