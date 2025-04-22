190071eae69c8ecaaca807c863acdebb7b6db33d
Matthias Carré <lemattheuxbis@gmail.com>
2025-04-22 23:28:44 +0200

    debut skybox

--- src/Skybox.js
+++ src/Skybox.js
@@ -9,11 +9,16 @@ export class SkyboxModel {
     }
 
     async load() {
-        const skybox = Mesh.CreateBox("skyBox", 5000.0, this.scene);
-        const skyboxMaterial = new StandardMaterial("skybox", this.scene);
-        skyboxMaterial.backFaceCulling = false;
-        skyboxMaterial.reflectionTexture = new CubeTexture("./../assets/skybox/skybox", this.scene);
-        skybox.material= skyboxMaterial;
+        try{
+            const skybox = Mesh.CreateBox("skyBox", 5000.0, this.scene);
+            const skyboxMaterial = new StandardMaterial("skybox", this.scene);
+            skyboxMaterial.backFaceCulling = false;
+            //skyboxMaterial.reflectionTexture = new CubeTexture("./assets/skybox/skybox", this.scene);
+            skybox.material= skyboxMaterial;
+        }catch (error) {
+            console.error("Error loading skybox model:", error);
+        }
+        
     }
 
 }
